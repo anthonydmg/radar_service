4c917218af43234a1cdeb452729f341b4dc4c16d
Anthony Miranda <anthonymirandagil@gmail.com>
2022-09-05 16:41:21 -0500

    []

--- bootloader.py
+++ bootloader.py
@@ -511,7 +511,7 @@ class Stm32Loader:
             self.write(b"\xff\xff\x00")
 
         previous_timeout_value = self.serial_connection.timeout
-        self.serial_connection.timeout = 100
+        self.serial_connection.timeout = 10
         self.serial_connection.connect()
         print("Extended erase (0x44), this can take ten seconds or more")
         try:
