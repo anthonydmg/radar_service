1e922cfa068fe3f61eb193c6bd0826f7eea552cb
Anthony Miranda <anthonymirandagil@gmail.com>
2022-09-19 09:29:03 -0500

    []

--- serial_comunication_radar.py
+++ serial_comunication_radar.py
@@ -365,7 +365,7 @@ def main():
 
 if __name__ == "__main__":
    com = init_service_radar_distance()
-   _polling_mode_distance(com, 0.2)
+   _polling_mode_distance(com, 1)
    # Wait for it to start
    #com.wait_start()
    #print('Sensor activated')
